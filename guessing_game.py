c22464527578464033c63bc8e76e00397e8b94cd
yomarisv <yomaris.verdin@ms223.org>
2016-04-05 09:30:33 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -7,3 +7,21 @@ while a != number:
 	a = input("Sorry guess again")
 print number
 #how do I get the range work
+name = raw_input("What is your name?")
+size = raw_input("What is the biggest number you want to guess?")
+import random
+number = random.randint(0, 10)
+a = input("Guess a number")
+counter = 1
+while a != number:
+	if a > number:
+		print "Sorry, " + str(name) + " your guess is too high"
+		counter = counter + 1
+	else:
+		print "Sorry, " + str(name) + " your guess is too low"
+		counter = counter + 1
+	a = input("Guess again?")
+	if a == number:
+		print "Correct"
+		print "you won"
+print (counter) 
